ac369949f09d38b866a0ecfc8d21644e7848b230
SeungsuKim <153834323+seungsu-K@users.noreply.github.com>
2024-07-04 15:03:28 +0900

    [level 0] Title: 컨트롤 제트, Time: 7.82 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = 0;
+    let arr = s.split(' ')
+    arr.forEach((item) => {
+        if(arr.includes('Z')) {
+            arr[arr.indexOf('Z')] = -arr[arr.indexOf('Z')-1];
+        } 
+        
+        answer += +item
+    })
+    console.log(arr)
+    return answer;
+}
