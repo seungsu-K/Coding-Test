af8b7d5f62a2c492d05409219537a86f5566d05f
SeungsuKim <153834323+seungsu-K@users.noreply.github.com>
2024-11-07 13:57:33 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,14 @@
 function solution(array, commands) {
     var answer = [];
     
-    commands.forEach((item) => {
-        answer.push(array.slice(item[0]-1, item[1]).sort((a,b) => a-b)[item[2]-1])
-    })
+    // commands.forEach((item) => {
+    //     answer.push(array.slice(item[0]-1, item[1]).sort((a,b) => a-b)[item[2]-1])
+    // })
+    
+    answer = commands.map((item) => 
+        array.slice(item[0]-1, item[1])
+            .sort((a,b) => a-b)[item[2]-1]
+    )
     
     return answer;
 }
