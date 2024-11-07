25cc728f9029accac3916131082c7d66b086fd88
SeungsuKim <153834323+seungsu-K@users.noreply.github.com>
2024-11-07 16:26:16 +0900

    [level 2] Title: H-Index, Time: 0.69 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -4,10 +4,10 @@ function solution(citations) {
     citations.sort((a,b) => b-a)
     
     for(let i = 0; i < citations.length; i++) {
+        if(citations[0] === 0) break;
+        if(citations[i] >= i+1) answer = i+1;
         
-        if(citations[i] === 0) break;
-        if(citations[i] >= i+1) answer = i+1
     }
-
+    
     return answer;
 }
